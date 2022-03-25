17d99f7ac54086154608299e8d54a2916d979eed
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-03-25 19:53:04 +0100

    Set components

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+   import Word from "./components/Word.svelte";
+   import Typer	from "./components/Typer.svelte";
+   import Score from "./components/Score.svelte";
+</script>
+<h1>Typing Speed</h1>
+<Word />
+<Typer />
+<Score />
